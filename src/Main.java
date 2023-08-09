8549904742d6a06e02b6923111e4a5120f9f85ed
arcad <arcadbak@rambler.ru>
2023-08-10 00:26:28 +0500

    Start

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,37 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import javax.swing.*;
+import java.time.Year;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("Домашнее задание №11");
+        System.out.println("Методы.");
+        int taskNumberRun;
+        try {
+            taskNumberRun = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "Какое задание нужно выполнить? (1-10)"));
+        } catch (Exception e) {
+            taskNumberRun = 0;
+            //e.printStackTrace(); // Выводит сообщение об ошибке
         }
+        switch (taskNumberRun) {
+            case 1 -> task1();    //Задание 1
+//            case 2 -> task2();    //Задание 2
+//            case 3 -> task3();    //Задание 3
+//            case 4 -> task4();    //Задание 4
+//            case 5 -> task5();    //Задание 5
+//            case 6 -> task6();    //Задание 6
+//            case 7 -> task7();    //Задание 7
+//            case 8 -> task8();    //Задание 8
+//            case 9 -> task9();    //Задание 9
+//            case 10 -> task10();    //Задание 10
+//            case 11 -> task11();    // Задание 12
+            default -> System.out.println("Вы не выбрали задание!?: " + taskNumberRun);
+        }
+        System.out.println("_________");
+        System.out.println("Домашка закончилась. Всем спасибо, все свободны.");
     }
+
+
+
 }
